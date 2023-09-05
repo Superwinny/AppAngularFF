a21bfb287b9948d3b0ef75e0c938e8ace173c559
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-05 10:10:55 +0200

    commit
    Add Service and routing page order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderPageComponent } from './order-page/order-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "order", component: OrderPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderPageComponent } from './order-page/order-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderPageComponent
   ],
   imports: [
     BrowserModule,

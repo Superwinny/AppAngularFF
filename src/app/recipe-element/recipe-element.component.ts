08ae9c560fe78540a202d1005b798e776b8fa1a0
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-06 14:34:14 +0200

    Commit
    Ajout Button , actions()

--- src/app/recipe-element/recipe-element.component.ts
+++ src/app/recipe-element/recipe-element.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RecipeInterface } from '../data.interface';
+
+type actionType = 'add' | 'remove';
 
 @Component({
   selector: 'app-recipe-element',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-element.component.scss']
 })
 export class RecipeElementComponent {
+@Input() recipe!: RecipeInterface;
+
+actions(type: actionType, payload: RecipeInterface ){
+console.log({type,payload});
+
+}
 
 }

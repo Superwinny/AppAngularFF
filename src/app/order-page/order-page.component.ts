ac5877f1f8cd6d863188c6e14ab7b75926afbb77
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-06 12:58:04 +0200

    Commit
    Correction de bug orderpage

--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService } from '../service/api.service';
+import { RestoCategoryInterface } from '../data.interface';
 
 @Component({
   selector: 'app-order-page',
@@ -8,17 +9,16 @@ import { APIService } from '../service/api.service';
 })
 export class OrderPageComponent implements OnInit {
 
+recipesCategories!: RestoCategoryInterface[];
+
 constructor(
-  private readonly ApiService : APIService
+  private readonly _ApiService : APIService
 ){
 
 }
  async ngOnInit() {
-
-  const data =  await  this.ApiService.getRecipes();
-
-  console.log(data);
-
+  const recipesCategories = await this._ApiService.getRecipes();
+  this.recipesCategories = recipesCategories;
 
 }
 

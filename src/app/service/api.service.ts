711f705ced09f4f72f7a3c62668b0ea9dbacabca
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-06 13:19:19 +0200

    commit

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
-import { RestoInterface } from '../data.interface';
+import { RestoInterface, RecipeInterface, RestoCategoryInterface } from '../data.interface';
 
 
 @Injectable({
@@ -14,12 +14,12 @@ export class APIService {
   ) { }
 
 
-   async getRecipes(){
+   async getRecipes(): Promise<RestoCategoryInterface[]>{
 
-    const url = `./assets/resto-data.json`
+    const url = `./assets/resto-data.json`;
     const resquest = this._Http.get<RestoInterface>(url);
-    const response = await firstValueFrom(resquest)
-    return response.data
+    const response = await firstValueFrom(resquest);
+    return response.data;
   }
 
   sendOrder(){}

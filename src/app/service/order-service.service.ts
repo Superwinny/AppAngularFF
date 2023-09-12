9d358ecb236441f291dd074ba437c65013452bfe
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-12 12:42:14 +0200

    commit

--- src/app/service/order-service.service.ts
+++ src/app/service/order-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { setDoc, doc, deleteDoc }from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderServiceService {
+  databaseName: string = 'recipeorder';
+  constructor(
+    private readonly _fireStore: Firestore
+  ) {
 
-  constructor() { }
+  }
+
+  async addOrder(data: {title: string}){
+
+    const docRef = doc(this._fireStore, this.databaseName + '/');
+    await setDoc(docRef, data)
+  }
+  async deleteOrder(){
+
+    const docRef = doc(this._fireStore, this.databaseName + '/');
+    await deleteDoc(docRef)
+  }
 }

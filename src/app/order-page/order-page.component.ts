b80e9c9fad5b960c40a6d547ee12af574caf180d
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-14 10:53:08 +0200

    commit
    Correction order page component

--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -12,79 +12,80 @@ import { OrderServiceService } from '../service/order-service.service';
 })
 export class OrderPageComponent implements OnInit {
 
-recipesCategories!: RestoCategoryInterface[];
-orderForm!: FormGroup;
+  recipesCategories!: RestoCategoryInterface[];
+  orderForm!: FormGroup;
 
-constructor(
-  private readonly _ApiService : APIService,
-  private readonly orderservice: OrderServiceService,
+  constructor(
+    private readonly _ApiService: APIService,
+    private readonly orderservice: OrderServiceService,
 
-){
+  ) {
 
-}
- async ngOnInit() {
-  const recipesCategories = await this._ApiService.getRecipes();
-  this.recipesCategories = recipesCategories;
-  this.orderForm = new FormGroup({
-    recipes: new FormArray([]),
-    dateTime: new FormControl()
-});
-
-}
-
-private _addRecipeToForm(recipeId: string, quantity: number){
-
-//get form Array Control to add selected recipe
-const formArray = this.orderForm?.get('recipes') as FormArray;
-
-const index = formArray.value.findIndex(
-(r:{recipeId: string; quantity: number;}) => r.recipeId === recipeId
-);
-if(index >= 0){
-  const quantityControler = formArray.at(index).get('quantity');
-  if(quantityControler){
-    quantityControler.setValue(quantityControler.value + quantity);
   }
-} else{
-// Build group element whith RecipeId and Quantity
-const group = new FormGroup({
-  recipeId: new FormControl("", Validators.compose([
- Validators.required
-])),
-quantity: new FormControl('', Validators.compose([
- Validators.required
-])),
-});
-//add recipe to arra form
-formArray.push(group);
-//display result in console
-console.log((this.orderForm.value));
-}
-}
+  async ngOnInit() {
+    const recipesCategories = await this._ApiService.getRecipes();
+    this.recipesCategories = recipesCategories;
+    this.orderForm = new FormGroup({
+      recipes: new FormArray([]),
+      dateTime: new FormControl(new Date)
+    });
 
-
-
-async actions($event: {type: string; payload?: any}) {
-  console.log($event);
-  switch (true) {
-    case $event.type === 'selectCategory':
-      console.log($event.type, $event.payload);
-      break;
-    case $event.type === 'add':
-      this._addRecipeToForm($event.payload.uuid, 1);
-      break;
-    case $event.type === 'remove':
-
-      break;
-    case $event.type === 'send-order':
-       await this.orderservice.saveData(this.orderForm.value);
-      (this.orderForm.get('recipes') as FormArray).clear();
-      console.log(this.orderForm.value);
-      alert('order sucessfuly send!');
-      break;
-    default:
-      break;
   }
-}
+
+  private _addRecipeToForm(recipeId: string, quantity: number) {
+
+    //get form Array Control to add selected recipe
+    const formArray = this.orderForm.get('recipes') as FormArray;
+
+    const index = formArray.value.findIndex(
+      (r: { recipeId: string; quantity: number; }) => r.recipeId === recipeId
+    );
+    if (index >= 0) {
+      const quantityControler = formArray.at(index).get('quantity');
+      if (quantityControler) {
+        quantityControler.setValue(quantityControler.value + quantity);
+      }
+    } else {
+      // Build group element whith RecipeId and Quantity
+      const group = new FormGroup({
+        recipeId: new FormControl(recipeId, Validators.compose([
+          Validators.required
+        ])),
+        quantity: new FormControl(quantity, Validators.compose([
+          Validators.required
+        ])),
+      });
+      //add recipe to arra form
+      formArray.push(group);
+      //display result in console
+
+    }
+    console.log((this.orderForm.value));
+  }
+
+
+
+  async actions($event: { type: string; payload?: any }) {
+    console.log($event);
+    switch (true) {
+      case $event.type === 'selectCategory':
+        console.log($event.type, $event.payload);
+        break;
+      case $event.type === 'add':
+        this._addRecipeToForm($event.payload.uuid, 1);
+        break;
+      case $event.type === 'remove':
+
+        break;
+      case $event.type === 'send-order':
+        await this.orderservice.saveData(this.orderForm.value);
+        (this.orderForm.get('recipes') as FormArray).clear();
+        console.log(this.orderForm.value);
+        alert('order sucessfuly send!');
+        break;
+      default:
+        break;
+    }
+  }
 
 }

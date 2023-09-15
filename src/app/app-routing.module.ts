792c30c79f6bca1e7f5cf1834ee04eff2fb42850
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-15 09:30:30 +0200

    commit
    comification routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderPageComponent } from './order-page/order-page.component';
+import { KitchenComponent } from './kitchen/kitchen.component';
 
 const routes: Routes = [
-  {path: "order", component: OrderPageComponent}
+  {path: "index", component: OrderPageComponent},
+  {path: "kitchen", component: KitchenComponent},
+  {path: "",redirectTo:'index', pathMatch: 'full'},
+
+
 ];
 
 @NgModule({
